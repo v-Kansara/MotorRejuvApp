751570d8c1f8b307742ff20e6b060f74c4b685aa
Vedant K <68434684+v-Kansara@users.noreply.github.com>
2024-01-30 18:50:03 -0500

    Add files via upload

---
+++ MedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedAppApp.swift
+//  MedApp
+//
+//  Created by Rakesh Kansara on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
